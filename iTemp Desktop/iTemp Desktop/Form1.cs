a13cccc12d38925df8b309f4ff233bdc17e61a17
ja74687 <piotr.krakos@gmail.com>
2017-08-19 15:29:38 +0200

    Add async metod

--- iTemp Desktop/iTemp Desktop/Form1.cs
+++ iTemp Desktop/iTemp Desktop/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,30 @@ namespace iTemp_Desktop
         public Form1()
         {
             InitializeComponent();
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.Maximum = 100;
+            StartProgress();
+        }
+
+       async void StartProgress()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i >= 99)
+                {
+                    i = 0;
+                }
+                progressBar1.Value = i;
+
+                await Task.Delay(2000);
+                label1.Text = Convert.ToString(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int i = 0;
+            label1.Text += " Klik";
         }
     }
 }
