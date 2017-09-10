3b9eab827055de0c4211d07688347aaa4b97bba4
ja74687 <piotr.krakos@gmail.com>
2017-09-10 20:14:23 +0200

    Add new layout and dowload information.

--- iTemp Desktop/iTemp Desktop/mainForm.Designer.cs
+++ iTemp Desktop/iTemp Desktop/mainForm.Designer.cs
@@ -30,21 +30,43 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pompaCWUONOFF = new System.Windows.Forms.TextBox();
+            this.trybPracyLabel = new System.Windows.Forms.TextBox();
+            this.pompaCOONOFF = new System.Windows.Forms.TextBox();
+            this.tempCWULabel = new System.Windows.Forms.Label();
+            this.tempCOLabel = new System.Windows.Forms.Label();
+            this.tempAlarmLabel = new System.Windows.Forms.Label();
             this.zadanaTempLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.licznikLabel = new System.Windows.Forms.Label();
             this.wykresCWU = new iTemp_Desktop.VerticalProgressBar();
             this.wykresCO = new iTemp_Desktop.VerticalProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pompaCWUONOFF);
+            this.groupBox1.Controls.Add(this.trybPracyLabel);
+            this.groupBox1.Controls.Add(this.pompaCOONOFF);
+            this.groupBox1.Controls.Add(this.tempCWULabel);
+            this.groupBox1.Controls.Add(this.tempCOLabel);
+            this.groupBox1.Controls.Add(this.tempAlarmLabel);
             this.groupBox1.Controls.Add(this.zadanaTempLabel);
             this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.wykresCWU);
             this.groupBox1.Controls.Add(this.wykresCO);
@@ -55,11 +77,82 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Aktualne Dane";
             // 
+            // pompaCWUONOFF
+            // 
+            this.pompaCWUONOFF.Enabled = false;
+            this.pompaCWUONOFF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pompaCWUONOFF.Location = new System.Drawing.Point(153, 220);
+            this.pompaCWUONOFF.Name = "pompaCWUONOFF";
+            this.pompaCWUONOFF.ReadOnly = true;
+            this.pompaCWUONOFF.ShortcutsEnabled = false;
+            this.pompaCWUONOFF.Size = new System.Drawing.Size(116, 20);
+            this.pompaCWUONOFF.TabIndex = 8;
+            this.pompaCWUONOFF.Text = "Pompa CWU: OFF";
+            this.pompaCWUONOFF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trybPracyLabel
+            // 
+            this.trybPracyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.trybPracyLabel.Location = new System.Drawing.Point(6, 269);
+            this.trybPracyLabel.Name = "trybPracyLabel";
+            this.trybPracyLabel.ReadOnly = true;
+            this.trybPracyLabel.Size = new System.Drawing.Size(263, 20);
+            this.trybPracyLabel.TabIndex = 8;
+            this.trybPracyLabel.Text = "Pompa CWU";
+            // 
+            // pompaCOONOFF
+            // 
+            this.pompaCOONOFF.Enabled = false;
+            this.pompaCOONOFF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pompaCOONOFF.Location = new System.Drawing.Point(7, 220);
+            this.pompaCOONOFF.Name = "pompaCOONOFF";
+            this.pompaCOONOFF.ReadOnly = true;
+            this.pompaCOONOFF.ShortcutsEnabled = false;
+            this.pompaCOONOFF.Size = new System.Drawing.Size(116, 20);
+            this.pompaCOONOFF.TabIndex = 8;
+            this.pompaCOONOFF.Text = "Pompa CO: OFF";
+            this.pompaCOONOFF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tempCWULabel
+            // 
+            this.tempCWULabel.BackColor = System.Drawing.Color.Transparent;
+            this.tempCWULabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tempCWULabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.tempCWULabel.Location = new System.Drawing.Point(182, 193);
+            this.tempCWULabel.Name = "tempCWULabel";
+            this.tempCWULabel.Size = new System.Drawing.Size(55, 22);
+            this.tempCWULabel.TabIndex = 7;
+            this.tempCWULabel.Text = "00°C";
+            this.tempCWULabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tempCOLabel
+            // 
+            this.tempCOLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tempCOLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tempCOLabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.tempCOLabel.Location = new System.Drawing.Point(29, 193);
+            this.tempCOLabel.Name = "tempCOLabel";
+            this.tempCOLabel.Size = new System.Drawing.Size(55, 22);
+            this.tempCOLabel.TabIndex = 7;
+            this.tempCOLabel.Text = "00°C";
+            this.tempCOLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tempAlarmLabel
+            // 
+            this.tempAlarmLabel.AutoSize = true;
+            this.tempAlarmLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tempAlarmLabel.ForeColor = System.Drawing.Color.Red;
+            this.tempAlarmLabel.Location = new System.Drawing.Point(105, 138);
+            this.tempAlarmLabel.Name = "tempAlarmLabel";
+            this.tempAlarmLabel.Size = new System.Drawing.Size(64, 28);
+            this.tempAlarmLabel.TabIndex = 7;
+            this.tempAlarmLabel.Text = "00°C";
+            // 
             // zadanaTempLabel
             // 
             this.zadanaTempLabel.AutoSize = true;
             this.zadanaTempLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.zadanaTempLabel.Location = new System.Drawing.Point(105, 95);
+            this.zadanaTempLabel.Location = new System.Drawing.Point(105, 67);
             this.zadanaTempLabel.Name = "zadanaTempLabel";
             this.zadanaTempLabel.Size = new System.Drawing.Size(64, 28);
             this.zadanaTempLabel.TabIndex = 7;
@@ -74,14 +167,36 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Temperatura CWU";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 125);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Temp. Alarmu";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(101, 73);
+            this.label3.Location = new System.Drawing.Point(101, 41);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(73, 26);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Zadana temp.";
+            this.label3.Text = "Zadana temp.\r\nCWU\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Wybrany tryb pracy pomp";
             // 
             // label1
             // 
@@ -94,6 +209,8 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
             this.groupBox2.Location = new System.Drawing.Point(295, 13);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(209, 316);
@@ -101,13 +218,21 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Ustawienia";
             // 
+            // licznikLabel
+            // 
+            this.licznikLabel.AutoSize = true;
+            this.licznikLabel.Location = new System.Drawing.Point(13, 336);
+            this.licznikLabel.Name = "licznikLabel";
+            this.licznikLabel.Size = new System.Drawing.Size(35, 13);
+            this.licznikLabel.TabIndex = 8;
+            this.licznikLabel.Text = "label6";
+            // 
             // wykresCWU
             // 
             this.wykresCWU.Location = new System.Drawing.Point(182, 41);
             this.wykresCWU.Name = "wykresCWU";
             this.wykresCWU.Size = new System.Drawing.Size(55, 149);
             this.wykresCWU.TabIndex = 5;
-            this.wykresCWU.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // wykresCO
             // 
@@ -115,13 +240,41 @@
             this.wykresCO.Name = "wykresCO";
             this.wykresCO.Size = new System.Drawing.Size(55, 149);
             this.wykresCO.TabIndex = 5;
-            this.wykresCO.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(149, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 31);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown2.Location = new System.Drawing.Point(149, 67);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 31);
+            this.numericUpDown2.TabIndex = 0;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(516, 352);
+            this.Controls.Add(this.licznikLabel);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -132,7 +285,11 @@
             this.Text = "iTemp Desktop";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,5 +303,16 @@
         private System.Windows.Forms.Label zadanaTempLabel;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label tempCOLabel;
+        private System.Windows.Forms.Label tempAlarmLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label tempCWULabel;
+        private System.Windows.Forms.TextBox pompaCOONOFF;
+        private System.Windows.Forms.TextBox pompaCWUONOFF;
+        private System.Windows.Forms.TextBox trybPracyLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label licznikLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
--- iTemp Desktop/iTemp Desktop/mainForm.cs
+++ iTemp Desktop/iTemp Desktop/mainForm.cs
@@ -1,29 +1,141 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using iTemp_Desktop.Function;
+using Newtonsoft.Json;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static iTemp_Desktop.Function.privateValues;
 
 namespace iTemp_Desktop
 {
     public partial class mainForm : Form
     {
+        string result = null;
         public mainForm()
         {
             InitializeComponent();
             zadanaTempLabel.Text = "00" + "°C";
+            getHttpAsync(urlIndex);
+            RunUpdateLoop();
         }
 
-        private void progressBar1_Click(object sender, EventArgs e)
+
+
+        async void RunUpdateLoop()
         {
-            wykresCO.Maximum = 100;
-            wykresCO.Value = 50;
+            int licznik = 0;
+            while (true)
+            {
+                if (licznik == 0)
+                {
+                    getHttpAsync(urlIndex);
+                    licznik = 20000;
+
+                }
+                licznikLabel.Text = "Odświerzenie za: " + Convert.ToString(licznik / 1000) + "s.";
+                licznik -= 1000;
+                await Task.Delay(1000);
+                //tekst.Text = string.Format("{0} ticks!", count++);
+            }
+
         }
+
+        void wczytajDane()
+        {
+            try
+            {
+                modelJson odebraneDane = JsonConvert.DeserializeObject<modelJson>(result);
+                wykresCO.Value = odebraneDane.tempCO;
+                tempCOLabel.Text = Convert.ToString(odebraneDane.tempCO) + "°C";
+                wykresCWU.Value = odebraneDane.tempCWU;
+                tempCWULabel.Text = Convert.ToString(odebraneDane.tempCWU) + "°C";
+                zadanaTempLabel.Text = Convert.ToString(odebraneDane.tempCWUZad) + "°C"; 
+                tempAlarmLabel.Text = Convert.ToString(odebraneDane.tempAlarm) + "°C"; 
+                if (odebraneDane.coPump == 1)
+                {
+                    pompaCOONOFF.BackColor = Color.LightGreen;
+                    pompaCOONOFF.Text = "Pompa CO: ON";
+                }
+                else
+                {
+                    pompaCOONOFF.BackColor = Color.LightPink;
+                    pompaCOONOFF.Text = "Pompa CO: OFF";
+                }
+                if (odebraneDane.cwuPump == 1)
+                {
+                    pompaCWUONOFF.BackColor = Color.LightGreen;
+                    pompaCWUONOFF.Text = "Pompa CWU: ON";
+                }
+                else
+                {
+                    pompaCWUONOFF.BackColor = Color.LightPink;
+                    pompaCWUONOFF.Text = "Pompa CWU: OFF";
+                }
+                /*
+    0--Tryb Letni
+    1--Priorytet CO
+    2--Priorytet CWU
+    3--Tryb reczny
+    4--Pompy CO i CWU
+   */
+                switch(odebraneDane.selectedMode){
+                    case 0:
+                        trybPracyLabel.Text = "Tryb Letni";
+                        break;
+                    case 1:
+                        trybPracyLabel.Text = "Priorytet CO";
+                        break;
+                    case 2:
+                        trybPracyLabel.Text = "Priorytet CWU";
+                        break;
+                    case 3:
+                        trybPracyLabel.Text = "Tryb reczny";
+                        break;
+                    case 4:
+                        trybPracyLabel.Text = "Pompy CO i CWU";
+                        break;
+
+                }
+                    
+
+            }
+            catch
+            {
+
+            }
+        }
+        async void getHttpAsync(string url)
+        {
+
+            using (HttpClientHandler ClientHandler = new HttpClientHandler())
+            using (HttpClient Client = new HttpClient(ClientHandler))
+            {
+                using (HttpResponseMessage ResponseMessage = await Client.GetAsync(url))
+                {
+                    if (ResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (HttpContent Content = ResponseMessage.Content)
+                        {
+                            result = await Content.ReadAsStringAsync();
+                            // tekst.Text = result;
+                            if (result != null)
+                            {
+                                wczytajDane();
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
     }
+
     public class VerticalProgressBar : System.Windows.Forms.ProgressBar
     {
         protected override CreateParams CreateParams
@@ -37,3 +149,5 @@ namespace iTemp_Desktop
         }
     }
 }
+
+
